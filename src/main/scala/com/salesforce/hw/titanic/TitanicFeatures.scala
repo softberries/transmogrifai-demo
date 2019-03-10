f12792418a19c4d4c7585295f567c77f2c361167
kris <krzysztof.grajek@softwaremill.com>
2019-03-10 13:59:54 +0100

    moving back to gradle

--- src/main/scala/com/salesforce/hw/titanic/TitanicFeatures.scala
+++ src/main/scala/com/salesforce/hw/titanic/TitanicFeatures.scala
@@ -28,25 +28,21 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.salesforce.hw.boston
+package com.salesforce.hw.titanic
 
 import com.salesforce.op.features.FeatureBuilder
 import com.salesforce.op.features.types._
 
-trait BostonFeatures extends Serializable {
-  val rowId = FeatureBuilder.Integral[BostonHouse].extract(_.rowId.toIntegral).asPredictor
-  val crim = FeatureBuilder.RealNN[BostonHouse].extract(_.crim.toRealNN).asPredictor
-  val zn = FeatureBuilder.RealNN[BostonHouse].extract(_.zn.toRealNN).asPredictor
-  val indus = FeatureBuilder.RealNN[BostonHouse].extract(_.indus.toRealNN).asPredictor
-  val chas = FeatureBuilder.PickList[BostonHouse].extract(x => Option(x.chas).toPickList).asPredictor
-  val nox = FeatureBuilder.RealNN[BostonHouse].extract(_.nox.toRealNN).asPredictor
-  val rm = FeatureBuilder.RealNN[BostonHouse].extract(_.rm.toRealNN).asPredictor
-  val age = FeatureBuilder.RealNN[BostonHouse].extract(_.age.toRealNN).asPredictor
-  val dis = FeatureBuilder.RealNN[BostonHouse].extract(_.dis.toRealNN).asPredictor
-  val rad = FeatureBuilder.Integral[BostonHouse].extract(_.rad.toIntegral).asPredictor
-  val tax = FeatureBuilder.RealNN[BostonHouse].extract(_.tax.toRealNN).asPredictor
-  val ptratio = FeatureBuilder.RealNN[BostonHouse].extract(_.ptratio.toRealNN).asPredictor
-  val b = FeatureBuilder.RealNN[BostonHouse].extract(_.b.toRealNN).asPredictor
-  val lstat = FeatureBuilder.RealNN[BostonHouse].extract(_.lstat.toRealNN).asPredictor
-  val medv = FeatureBuilder.RealNN[BostonHouse].extract(_.medv.toRealNN).asResponse
+trait TitanicFeatures extends Serializable {
+  val survived = FeatureBuilder.RealNN[Passenger].extract(_.getSurvived.toDouble.toRealNN).asResponse
+  val pClass = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getPclass).map(_.toString).toPickList).asPredictor // scalastyle:off
+  val name = FeatureBuilder.Text[Passenger].extract(d => Option(d.getName).toText).asPredictor
+  val sex = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getSex).toPickList).asPredictor
+  val age = FeatureBuilder.Real[Passenger].extract(d => Option(Double.unbox(d.getAge)).toReal).asPredictor
+  val sibSp = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getSibSp).map(_.toString).toPickList).asPredictor
+  val parch = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getParch).map(_.toString).toPickList).asPredictor
+  val ticket = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getTicket).toPickList).asPredictor
+  val fare = FeatureBuilder.Real[Passenger].extract(d => Option(Double.unbox(d.getFare)).toReal).asPredictor
+  val cabin = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getCabin).toPickList).asPredictor
+  val embarked = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getEmbarked).toPickList).asPredictor
 }
